d81261643d0b73289975fdbd5e55941e1fb47ecd
jesse-peraza <perazajesse@yahoo.com>
2023-08-18 01:41:56 -0700

    edit and update actions created

--- routes/skills.js
+++ routes/skills.js
@@ -5,8 +5,10 @@ const skillsCtrl = require('../controllers/skills');
 router.get('/', skillsCtrl.index);
 router.get('/new', skillsCtrl.new);
 router.get('/:id', skillsCtrl.show);
+router.get('/:id/edit', skillsCtrl.edit);
 router.get('/', skillsCtrl.create);
 router.delete('/:id', skillsCtrl.delete);
+router.put('/:id', skillsCtrl.update);
 
 
 module.exports = router;
